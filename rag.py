dd50b7c412fbc87a2f3bb41b67b2fa9cc7af00ce
Duy Huynh <vndee.huynh@gmail.com>
2023-12-01 17:31:11 +0700

    Add local-rag-example

--- rag.py
+++ rag.py
@@ -1,60 +1,50 @@
-from langchain.prompts import ChatPromptTemplate
-from langchain.document_loaders import UnstructuredPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma, Qdrant
-from langchain.chat_models import ChatOllama, ChatOpenAI
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnablePassthrough
+from langchain import hub
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOllama
 from langchain.embeddings import FastEmbedEmbeddings
-from langchain.chains import ConversationalRetrievalChain
+from langchain.schema.output_parser import StrOutputParser
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.vectorstores.utils import filter_complex_metadata
 
 
 class ChatPDF:
+    vector_store = None
+    retriever = None
+    chain = None
+
     def __init__(self):
-        self.embedding = None
-        self.vector_store = None
-        self.retriever = None
-        self.chain = None
-
         self.model = ChatOllama(model="mistral")
-
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-        self.init_embedding()
-        self.prompt = ChatPromptTemplate.from_template(
-            "Answer questions about a PDF document based the retrieved documents below:\n{context}\n"
-            "Question: {question}",
-        )
-
-    def init_embedding(self):
-        self.embedding = FastEmbedEmbeddings()
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
+        self.prompt = hub.pull("rlm/rag-prompt-mistral")
 
     def ingest(self, pdf_file_path: str):
-        loader = UnstructuredPDFLoader(
-            pdf_file_path,
-            mode="elements",
-            strategy="fast"
-        )
-        docs = loader.load()
+        docs = PyPDFLoader(file_path=pdf_file_path).load()
         chunks = self.text_splitter.split_documents(docs)
+        chunks = filter_complex_metadata(chunks)
 
-        vector_store = Qdrant.from_documents(
-            chunks,
-            self.embedding,
-            location=":memory:",
-            collection_name="pdf"
+        vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+        self.retriever = vector_store.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={
+                "k": 3,
+                "score_threshold": 0.5,
+            },
         )
-        self.retriever = vector_store.as_retriever()
 
-        self.chain = ConversationalRetrievalChain.from_llm(self.model, self.retriever)
+        self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+                      | self.prompt
+                      | self.model
+                      | StrOutputParser())
 
     def ask(self, query: str):
         if not self.chain:
             return "Please, add a PDF document first."
 
-        return self.chain({"question": query, "chat_history": ""})
+        return self.chain.invoke(query)
 
-
-if __name__ == "__main__":
-    chat = ChatPDF()
-    chat.ingest("cidr2021_paper17.pdf")
-    print(chat.ask("Who is the authors?"))
+    def clear(self):
+        self.vector_store = None
+        self.retriever = None
+        self.chain = None
