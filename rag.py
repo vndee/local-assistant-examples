73105ca01d1070653122c54115ed9267d439b0dd
Wu, Yang <yang.wu2@Honeywell.com>
2024-06-25 13:16:11 +0800

    版本修改

--- rag.py
+++ rag.py
@@ -15,7 +15,7 @@ class ChatPDF:
     chain = None
 
     def __init__(self):
-        self.model = ChatOllama(model="mistral")
+        self.model = ChatOllama(model="qwen2")
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
         self.prompt = PromptTemplate.from_template(
             """
